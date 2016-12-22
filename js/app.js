f9faf956eabca40fd6b940619894f1735b6aca3f
Vimbai <vimbai.dzimwasha@gmail.com>
2016-12-22 17:24:40 +0000

    Introduce new styles and accessibility related code
    Also introduce grid and clean up some of the existing code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-$(function(){
-   $(document).ready(function() {
-  $('.nav').on('click', function() {
-    $('.nav, .intro-overlay').toggleClass('active');
-  });
-});
-});
+    $(document).ready(function() {
+        $('#intro-toggle').on('click', function(e) {
+        	console.log('running out of time');
+            var $toggle = $(this);
+            var $menu = $(('.intro-overlay'));
+
+            if ($menu.attr('aria-hidden') == 'true') {
+                $('.nav, main, .intro-overlay').addClass('active');
+                $menu.attr('aria-hidden', 'false');
+                $toggle.attr('aria-expanded', 'true');
+                $toggle.attr('aria-label', 'close about me section');
+                $('.intro-state').text('Close About Me Section');
+            } else if ($menu.attr('aria-hidden') == 'false') {
+                $('.nav, main,  .intro-overlay').removeClass('active');
+                $menu.attr('aria-hidden', 'true');
+                $toggle.attr('aria-expanded', 'false');
+                $toggle.attr('aria-label', 'open about me section');
+                $('.intro-state').text('Open About Me Section');
+            }
+        });
+    });
